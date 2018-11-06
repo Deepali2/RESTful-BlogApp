6936be631f01bc72028813d003a687452ba3dda4
Deepali2 <hdgarg@gmail.com>
2018-11-06 11:24:13 -0800

    adds the RESTful routes INDEX, NEW, CREATE

---
+++ app.js
@@ -0,0 +1,76 @@
+const express = require("express"),
+      bodyParser = require("body-parser"),
+      mongoose = require("mongoose"),
+      ejs = require("ejs");
+
+
+const app = express();
+
+//APP CONFIG
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+mongoose.connect("mongodb://localhost:27017/restful_blog_app", {useNewUrlParser: true});
+
+//MONGOOSE MODEL CONFIG
+const blogSchema = new mongoose.Schema({
+  title: String,
+  image: String,
+  body: String,
+  created: {type: Date, default: Date.now}
+});
+
+const Blog = mongoose.model("Blog", blogSchema);
+
+//RESTFUL ROUTES
+// Blog.create(
+//   {
+//     title: "Test Blog",
+//     image: "https://images.unsplash.com/photo-1537215781824-73d7761029e1?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ae476497bd08d1b86d74bf2e561c132&auto=format&fit=crop&w=900&q=60",
+//     body: "Make this wonderful looking yummy salad and get a flat tummy in the process",
+    
+//   }, function(err, blog) {
+//     if (err) console.log("There was an error in creating the blog ", err);
+//     else console.log("A new blog has been created: ", blog);
+//   }
+// )
+
+//INDEX RESTFUL ROUTE
+app.get("/blogs", function(req, res) {
+  Blog.find({}, function(err, blogs) {
+   if (err) console.log("Error in getting all the blogs: ", err);
+   else res.render("index", {blogs: blogs});
+  });  
+});
+
+//NEW ROUTE
+app.get("/blogs/new", function(req, res) {
+  res.render("new");
+});
+
+//CREATE ROUTE
+app.post("/blogs", function(req, res) { 
+  Blog.create(req.body.blog, function(err, blog) {
+    if (err) {
+      console.log("There is an error in creating a new blog: ", err);
+      res.render("new");
+    }
+    else {
+      console.log("New blog created: ", blog);
+      res.redirect("/blogs");
+    }
+  });
+});
+
+
+app.get("/", function(req, res) {
+  res.redirect("/blogs");
+});
+
+app.get("*", function(req, res) {
+  res.send("Hi! This route does not exist");
+});
+
+app.listen(3000, function() {
+  console.log('Listening on port 3000');
+});
